7d88824e3f40e7bbbe21b2efdb3581d909b8dfb9
Gustavo Mahecha <gmahechas@outlook.com>
2024-04-22 16:52:00 -0500

    vault

---
+++ vault/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# fix permissions 
+chown -R vault:vault /vault/config
+chown -R vault:vault /vault/logs
+chown -R vault:vault /vault/data
+
+# start vault
+exec vault server -config=/vault/config/vault.hcl
