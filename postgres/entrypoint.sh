c5b936e054c32ec2d8b9e4570423d92cc3b9c2c1
Gustavo Mahecha <gmahechas@outlook.com>
2024-04-23 17:55:51 -0500

    postgres

--- postgres/entrypoint.sh
+++ postgres/entrypoint.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 set -e  # exit on error
 
-# run
-exec /usr/local/bin/docker-entrypoint.sh "$@"
-
 # change permissions
 chown postgres:postgres /var/lib/postgresql/data/ssl/server.key
 chmod 600 /var/lib/postgresql/data/ssl/server.key
 
 # include custom configs
-PG_CONF="/var/lib/postgresql/data/pgdata/postgresql.conf"
-PG_HBA="/var/lib/postgresql/data/pgdata/pg_hba.conf"
-echo "include = 'postgresql.custom.conf'" >> "$PG_CONF"
-echo "include = 'pg_hba.custom.conf'" >> "$PG_HBA"
+echo "include = 'postgresql.custom.conf'" >> /var/lib/postgresql/data/pgdata/postgresql.conf
+echo "include = 'pg_hba.custom.conf'" >> /var/lib/postgresql/data/pgdata/pg_hba.conf
 
-wait
+# run
+exec /usr/local/bin/docker-entrypoint.sh "$@"
