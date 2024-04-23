e0f7a5c31795ccbbe9db6962e7d2eb883c091522
Gustavo Mahecha <gmahechas@outlook.com>
2024-04-22 22:37:01 -0500

    vault

--- vault/config/postgres/start.sh
+++ vault/config/postgres/start.sh
@@ -1,21 +1,23 @@
 #!/bin/sh
 
 # secrets
+echo "###### enabling secrets ######"
 vault secrets enable -version=2 -description=kv kv
 
-echo "creating secrets"
+echo "###### creating secrets ######"
 # postgres
 vault kv put kv/databases/postgres password=postgres
 
 
 # policies
-echo "creating policies"
+echo "###### creating policies ######"
 vault policy write postgres postgres.hcl
 
 # approle
+echo "###### enabling approle auth ######"
 vault auth enable approle
 
-echo "enable approle for postgres"
+echo "###### creating approle for postgres ######"
 vault write auth/approle/role/postgres policies=postgres
 vault read auth/approle/role/postgres/role-id
 vault write -f auth/approle/role/postgres/secret-id
--- vault/consul/config.hcl
+++ vault/consul/config.hcl
@@ -1,6 +1,5 @@
 vault {
   address = "http://10.1.0.228:8200"
-  enabled = true
   renew_token = false
   ssl {
     enabled = false
@@ -8,8 +7,9 @@ vault {
 }
 
 template {
-  source      = "/templates/postgres-config.tpl"
+  source      = "/templates/postgres-config.ctmpl"
   destination = "/run/secrets/postgres_password"
+	create_dest_dirs = true
   perms       = 0644
   command     = "echo 'Template processed'"
   command_timeout = "30s"
