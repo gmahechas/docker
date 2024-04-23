4baac8a9940172a375c8414eb1bf38e38653d488
Gustavo Mahecha <gmahechas@outlook.com>
2024-04-23 14:33:19 -0500

    postgres

--- vault/config/postgres/start.sh
+++ vault/config/postgres/start.sh
@@ -2,31 +2,22 @@
 
 # secrets
 echo "###### enabling secrets ######"
-vault secrets enable -version=2 -description=kv kv
+vault secrets enable database
 
-echo "###### creating secrets ######"
 # postgres
-vault kv put kv/databases/postgres password=postgres
+echo "###### creating secrets ######"
+vault write database/config/postgresql \
+      plugin_name=postgresql-database-plugin \
+      allowed_roles=readonly \
+      connection_url="postgresql://{{username}}:{{password}}@$POSTGRES_URL/myapp?sslmode=disable" \
+      username=root \
+      password=rootpassword
 
 
-# policies
-echo "###### creating policies ######"
-vault policy write postgres postgres.hcl
+vault write database/roles/readonly db_name=postgresql \
+        creation_statements=@readonly.sql \
+        default_ttl=1h max_ttl=24h
+			
+vault policy write db_creds db_creds.hcl
 
-# approle
-echo "###### enabling approle auth ######"
-vault auth enable approle
-
-echo "###### creating approle for postgres ######"
-vault write auth/approle/role/postgres policies=postgres
-vault read auth/approle/role/postgres/role-id
-vault write -f auth/approle/role/postgres/secret-id
-
-# userpass
-echo "###### enabling userpass auth ######"
-vault auth enable userpass
-
-echo "###### creating userpass for consul ######"
-vault write auth/userpass/users/consul password=consul policies=postgres
-vault list auth/userpass/users
-vault read auth/userpass/users/consul
+# DB_TOKEN=$(vault token create -policy="db_creds" -format json | jq -r '.auth | .client_token')
