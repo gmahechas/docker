8577bcbc6d044dd6f25fe67a431e14ca810a1090
Gustavo Mahecha <gmahechas@outlook.com>
2024-04-23 22:52:47 -0500

    postgres

--- vault/config/postgres/postgres.hcl
+++
@@ -1,3 +0,0 @@
-path "kv/data/databases/postgres" {
-  capabilities = ["read"]
-}
--- vault/config/postgres/start.sh
+++ vault/config/postgres/start.sh
@@ -18,6 +18,9 @@ vault write database/roles/readonly db_name=postgresql \
         creation_statements=@readonly.sql \
         default_ttl=1h max_ttl=24h
 			
-vault policy write db_creds db_creds.hcl
+vault policy write database_credentials database_credentials.hcl # it should be for all database credentials
+#vault policy write db_creds db_creds.hcl
+
 
 # DB_TOKEN=$(vault token create -policy="db_creds" -format json | jq -r '.auth | .client_token')
+# vault token create -policy="db_creds" -format json
