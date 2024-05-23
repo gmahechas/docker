a34ec7ad7068aefb0b80dd1b3fb480d65b7d90b9
Gustavo Mahecha <gmahechas@outlook.com>
2024-05-22 21:01:15 -0500

    mongodb

--- mongodb/ssl/init-scripts/start.sh
+++ mongodb/ssl/init-scripts/start.sh
@@ -2,9 +2,9 @@
 
 # check if replica set is initialized
 if [ -f /data/db/replica_set_initialized ]; then
-  echo "Replica set already initialized. Starting MongoDB with authentication..."
-  exec mongod --replSet rs0 --bind_ip_all --port $PORT --tlsMode requireTLS --tlsCertificateKeyFile /etc/ssl/$HOSTNAME/$HOSTNAME.pem --tlsCAFile /etc/ssl/ca.crt --auth
+  echo "replica set already initialized. starting mongodb with authentication..."
+  exec mongod --replSet rs0 --bind_ip_all --port "$PORT" --tlsMode requireTLS --tlsCertificateKeyFile "/etc/ssl/$HOSTNAME/$HOSTNAME.pem" --tlsCAFile "/etc/ssl/ca.crt" --auth
 else
-  echo "Replica set not initialized. Starting MongoDB without authentication..."
-  exec mongod --replSet rs0 --bind_ip_all --port $PORT --tlsMode requireTLS --tlsCertificateKeyFile /etc/ssl/$HOSTNAME/$HOSTNAME.pem --tlsCAFile /etc/ssl/ca.crt
+  echo "replica set not initialized. starting mongodb without authentication..."
+  exec mongod --replSet rs0 --bind_ip_all --port "$PORT" --tlsMode requireTLS --tlsCertificateKeyFile "/etc/ssl/$HOSTNAME/$HOSTNAME.pem" --tlsCAFile "/etc/ssl/ca.crt"
 fi
