5711226334606fe02a1eec200b5f8e03d2eb3eda
Gustavo Mahecha <gmahechas@outlook.com>
2024-05-22 21:09:21 -0500

    mongodb

--- mongodb/ssl/init-scripts/start.sh
+++ mongodb/ssl/init-scripts/start.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+export PORT=${PORT}
+export HOSTNAME=${HOSTNAME}#
+
 # check if replica set is initialized
 if [ -f /data/db/replica_set_initialized ]; then
   echo "replica set already initialized. starting mongodb with authentication..."
-  exec mongod --replSet rs0 --bind_ip_all --port "${PORT}" --tlsMode requireTLS --tlsCertificateKeyFile "/etc/ssl/${HOSTNAME}/${HOSTNAME}.pem" --tlsCAFile "/etc/ssl/ca.crt" --auth
+  COMMAND="mongod --replSet rs0 --bind_ip_all --port ${PORT} --tlsMode requireTLS --tlsCertificateKeyFile /etc/ssl/${HOSTNAME}/${HOSTNAME}.pem --tlsCAFile /etc/ssl/ca.crt --auth"
+  echo $COMMAND
+  exec $COMMAND
 else
   echo "replica set not initialized. starting mongodb without authentication..."
-  exec mongod --replSet rs0 --bind_ip_all --port "${PORT}" --tlsMode requireTLS --tlsCertificateKeyFile "/etc/ssl/${HOSTNAME}/${HOSTNAME}.pem" --tlsCAFile "/etc/ssl/ca.crt"
+  COMMAND="mongod --replSet rs0 --bind_ip_all --port ${PORT} --tlsMode requireTLS --tlsCertificateKeyFile /etc/ssl/${HOSTNAME}/${HOSTNAME}.pem --tlsCAFile /etc/ssl/ca.crt"
+  echo $COMMAND
+  exec $COMMAND
 fi
