59d89a8961f323da285449a925cc164f20fdcd22
Gustavo Mahecha <gmahechas@outlook.com>
2024-05-22 20:05:03 -0500

    mongodb

--- mongodb/ssl/init-scripts/init.sh
+++ mongodb/ssl/init-scripts/init.sh
@@ -1,37 +1,38 @@
 #!/bin/bash
 
-# Esperar a que mongodb1 esté listo
+# waiting for mongodb1 to be ready
 while ! mongo --tls --tlsCAFile /etc/ssl/ca.crt --tlsCertificateKeyFile /etc/ssl/client/client.pem --host mongodb1 --port 30001 --eval 'db.runCommand({ping: 1})'; do
-  echo 'Waiting for mongodb1 to be ready...'
+  echo 'waiting for mongodb1 to be ready...'
   sleep 5
 done;
 
-# Iniciar el replica set
-echo 'Initiating replica set...'
+# initiating replica set
+echo 'initiating replica set...'
 mongo --tls --tlsCAFile /etc/ssl/ca.crt --tlsCertificateKeyFile /etc/ssl/client/client.pem --host mongodb1 --port 30001 --eval 'rs.initiate({_id: "rs0", members: [{_id: 0, host: "mongodb1:30001"}, {_id: 1, host: "mongodb2:30002"}, {_id: 2, host: "mongodb3:30003"}]});'
+mongo --tls --tlsCAFile /etc/ssl/ca.crt --tlsCertificateKeyFile /etc/ssl/client/client.pem --host mongodb1 --port 30001 --eval 'rs.status()'
 
-# Esperar a que el replica set esté listo y un nodo sea elegido como PRIMARY
-echo 'Waiting for replica set to initialize...'
+# Waiting for replica set to initialize and a primary node to be elected
+echo 'waiting for replica set to initialize...'
 PRIMARY_READY=0
 while [ $PRIMARY_READY -eq 0 ]; do
   PRIMARY_STATE=$(mongo --tls --tlsCAFile /etc/ssl/ca.crt --tlsCertificateKeyFile /etc/ssl/client/client.pem --host mongodb1 --port 30001 --quiet --eval 'rs.status().members.filter(m => m.stateStr == "PRIMARY").length')
   if [ "$PRIMARY_STATE" -eq 1 ]; then
     PRIMARY_READY=1
-    echo 'Primary node is ready.'
+    echo 'primary node is ready.'
   else
-    echo 'Waiting for a primary node to be elected...'
+    echo 'waiting for a primary node to be elected...'
     sleep 5
   fi
 done
 
-# Crear usuario administrador
-echo 'Creating admin user...'
-mongo --tls --tlsCAFile /etc/ssl/ca.crt --tlsCertificateKeyFile /etc/ssl/client/client.pem --host mongodb1 --port 30001 --eval 'db.getSiblingDB("admin").createUser({user: "root", pwd: "root", roles: [{role: "root", db: "admin"}]});'
-echo 'Admin user created.'
+# creating root user
+# echo 'creating root user...'
+# mongo --tls --tlsCAFile /etc/ssl/ca.crt --tlsCertificateKeyFile /etc/ssl/client/client.pem --host mongodb1 --port 30001 --eval 'db.getSiblingDB("admin").createUser({user: "root", pwd: "root", roles: [{role: "root", db: "admin"}]});'
+# echo 'root user created.'
 
-# Habilitar autenticación en todos los nodos
-echo 'Enabling authentication...'
-for NODE in mongodb1 mongodb2 mongodb3; do
-  mongod --replSet rs0 --bind_ip_all --port 30001 --tlsMode requireTLS --tlsCertificateKeyFile /etc/ssl/$NODE/$NODE.pem --tlsCAFile /etc/ssl/ca.crt --auth &
-done
-echo 'Authentication enabled.'
+# enabling authentication
+# echo 'enabling authentication...'
+# for NODE in mongodb1 mongodb2 mongodb3; do
+#   mongod --replSet rs0 --bind_ip_all --port 30001 --tlsMode requireTLS --tlsCertificateKeyFile /etc/ssl/$NODE/$NODE.pem --tlsCAFile /etc/ssl/ca.crt --auth &
+# done
+# echo 'authentication enabled.'
