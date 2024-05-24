8827a0feb9d902359fc6741d994bcdc8809e8a10
Gustavo Mahecha <gmahechas@outlook.com>
2024-05-23 21:52:09 -0500

    mongodb

--- mongodb/ssl/scripts/healthcheck.sh
+++ mongodb/ssl/scripts/healthcheck.sh
@@ -6,6 +6,7 @@ MONGO_CA_FILE=/etc/ssl/ca.crt
 MONGO_CERT_FILE=/etc/ssl/client/client.pem
 
 STATUS=$(echo 'rs.status().members.find(m => m.name === "'$MONGO_HOST':'$MONGO_PORT'" && (m.stateStr === "PRIMARY" || m.stateStr === "SECONDARY")) ? 1 : 0' | mongo --tls --tlsCAFile $MONGO_CA_FILE --tlsCertificateKeyFile $MONGO_CERT_FILE --host $MONGO_HOST --port $MONGO_PORT --quiet)
+#STATUS=$(echo 'rs.status().members.find(m => m.name === "'$MONGO_HOST':'$MONGO_PORT'" && (m.stateStr === "PRIMARY" || m.stateStr === "SECONDARY")) ? 1 : 0' | mongo --tls --tlsCAFile $MONGO_CA_FILE --tlsCertificateKeyFile $MONGO_CERT_FILE --host $MONGO_HOST --port $MONGO_PORT -u root -p root --authenticationDatabase admin --quiet)
 
 if [ "$STATUS" -eq 1 ]; then
   exit 0
