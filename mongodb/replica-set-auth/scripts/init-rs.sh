a5d2d822c742329fa6bf226b64078e52fbd3ab32
Gustavo Mahecha <gmahechas@outlook.com>
2024-05-26 15:01:32 +0000

    mongodb

---
+++ mongodb/replica-set-auth/scripts/init-rs.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+MONGO_CMD="mongo --host mongodb1.gmahechas.local"
+
+# waiting for mongodb1.gmahechas.local to be ready
+while ! $MONGO_CMD --eval 'db.runCommand({ping: 1})'; do
+  echo 'waiting for mongodb1.gmahechas.local to be ready...'
+  sleep 5
+done;
+
+# initiating replica set
+echo 'initiating replica set...'
+$MONGO_CMD --eval 'rs.initiate({_id: "rs0", members: [{_id: 0, host: "mongodb1.gmahechas.local"}, {_id: 1, host: "mongodb2.gmahechas.local"}, {_id: 2, host: "mongodb3.gmahechas.local"}]});'
+
+# Waiting for replica set to initialize and a primary node to be elected
+echo 'waiting for replica set to initialize...'
+PRIMARY_READY=0
+while [ $PRIMARY_READY -eq 0 ]; do
+  PRIMARY_STATE=$($MONGO_CMD --quiet --eval 'rs.status().members.filter(m => m.stateStr == "PRIMARY").length')
+  if [ "$PRIMARY_STATE" -eq 1 ]; then
+    echo 'primary node is ready.'
+    PRIMARY_READY=1
+  else
+    echo 'waiting for a primary node to be elected...'
+    sleep 5
+  fi
+done
+
+exit 0
