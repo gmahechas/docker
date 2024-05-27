e1e5dc28f7663f91a4835785897e1fc421aa0bd0
Gustavo Mahecha <gmahechas@outlook.com>
2024-05-27 03:27:32 +0000

    mongodb

--- mongodb/replica-set-auth-ssl/scripts/init-rs.sh
+++ mongodb/replica-set-auth-ssl/scripts/init-rs.sh
@@ -16,7 +16,7 @@ done;
 
 # initiating replica set
 echo 'initiating replica set...'
-$MONGODB_CMD --eval 'rs.initiate({_id: "rs0", members: [{_id: 0, host: "mongodb1.gmahechas.local"}, {_id: 1, host: "mongodb2.gmahechas.local"}, {_id: 2, host: "mongodb3.gmahechas.local"}]});'
+$MONGODB_CMD --eval 'rs.initiate({_id: "rs0", members: [{_id: 0, host: "mongodb1.gmahechas.local:27017"}, {_id: 1, host: "mongodb2.gmahechas.local:27017"}, {_id: 2, host: "mongodb3.gmahechas.local:27017"}]});'
 
 # Waiting for replica set to initialize and a primary node to be elected
 echo 'waiting for replica set to initialize...'
