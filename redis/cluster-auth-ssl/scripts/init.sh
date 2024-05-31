c728895d2131605ef4e184e35e77832b5d99ba80
Gustavo Mahecha <gmahechas@outlook.com>
2024-05-31 03:54:16 +0000

    redis

--- redis/cluster-auth-ssl/scripts/init.sh
+++ redis/cluster-auth-ssl/scripts/init.sh
@@ -4,15 +4,15 @@ REDISDB_ANNOUNCE_IP=$1
 REDISDB_NODE=$2
 
 REDISDB_CMD="redis-server \
-  --cluster-enabled yes \
-  --cluster-config-file nodes.conf \
-  --cluster-node-timeout 5000 \
   --appendonly yes \
   --requirepass root \
   --masterauth root \
   --aclfile /etc/config/acl.conf \
-  --cluster-announce-ip $REDISDB_ANNOUNCE_IP \
   --port 0 \
+  --cluster-enabled yes \
+  --cluster-config-file nodes.conf \
+  --cluster-node-timeout 5000 \
+  --cluster-announce-ip $REDISDB_ANNOUNCE_IP \
   --tls-port 6380 \
   --tls-cert-file /etc/ssl/$REDISDB_NODE/$REDISDB_NODE.crt \
   --tls-key-file /etc/ssl/$REDISDB_NODE/$REDISDB_NODE.key \
