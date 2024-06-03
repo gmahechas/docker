b06f10ef27ac3e565ca78c214d77c8070c4b8671
Gustavo Mahecha <gmahechas@outlook.com>
2024-06-03 03:20:35 +0000

    redis

--- redis/cluster-auth-ssl/scripts/init-cluster.sh
+++ redis/cluster-auth-ssl/scripts/init-cluster.sh
@@ -2,6 +2,6 @@
 
 sleep 10
 
-echo "yes" | redis-cli -a root --user root -p 6378 -h redisdb1.gmahechas.local --tls --cacert /etc/ssl/ca.crt --cert /etc/ssl/redisdb/redisdb.crt --key /etc/ssl/redisdb/redisdb.key --cluster create 172.28.0.2:6378 172.28.0.3:6378 172.28.0.4:6378 --cluster-replicas 0
+echo "yes" | redis-cli -a root --user root -p 6380 -h redisdb1.gmahechas.local --tls --cacert /etc/ssl/ca.crt --cert /etc/ssl/redisdb/redisdb.crt --key /etc/ssl/redisdb/redisdb.key --cluster create 172.28.0.2:6380 172.28.0.3:6380 172.28.0.4:6380 --cluster-replicas 0
 
 exit 0
