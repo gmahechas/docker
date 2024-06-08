06d20a696a5769240c121be752853c91711c4c5d
Gustavo Mahecha <gmahechas@outlook.com>
2024-06-08 12:45:44 +0000

    redis

--- mongodb/replica-set-auth-ssl/scripts/init-rs.sh
+++ mongodb/replica-set-auth-ssl/scripts/init-rs.sh
@@ -18,7 +18,7 @@ done
 
 # initiating replica set
 echo 'initiating replica set...'
-$MONGODB_CMD --eval 'rs.initiate({_id: "rs0", members: [{_id: 0, host: "mongodb1.gmahechas.local:27017", priority: 2}, {_id: 1, host: "mongodb2.gmahechas.local:27017", priority: 1}, {_id: 2, host: "mongodb3.gmahechas.local:27017", priority: 1}]});'
+$MONGODB_CMD --eval 'rs.initiate({_id: "rs0", members: [{_id: 0, host: "mongodb1.gmahechas.local:27017", priority: 2}, {_id: 1, host: "mongodb2.gmahechas.local:28017", priority: 1}, {_id: 2, host: "mongodb3.gmahechas.local:29017", priority: 1}]});'
 
 # Waiting for replica set to initialize and a primary node to be elected
 echo 'waiting for replica set to initialize...'
--- mongodb/replica-set-auth-ssl/scripts/init.sh
+++ mongodb/replica-set-auth-ssl/scripts/init.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+MONGODB_PORT=$1
 MONGODB_INIT_FLAG=/etc/config/.mongodb_init_flag
-MONGODB_CMD="mongod --replSet rs0 --bind_ip_all --tlsMode requireTLS --tlsCAFile /etc/ssl/ca.crt --tlsCertificateKeyFile /etc/ssl/mongodb/mongodb.pem"
+MONGODB_CMD="mongod --replSet rs0 --bind_ip_all --tlsMode requireTLS --tlsCAFile /etc/ssl/ca.crt --tlsCertificateKeyFile /etc/ssl/mongodb/mongodb.pem --port $MONGODB_PORT"
 
 if [ -s "$MONGODB_INIT_FLAG" ]; then
 	MONGODB_CMD="$MONGODB_CMD --keyFile /etc/config/mongodb-keyfile"
