5b83f6d87fc2c78c713dee73e94c2b86f3c4d5de
Gustavo Mahecha <gmahechas@outlook.com>
2024-06-09 23:25:00 -0500

    mongodb

--- mongodb/replica-set-auth-ssl/scripts/init.sh
+++ mongodb/replica-set-auth-ssl/scripts/init.sh
@@ -2,7 +2,7 @@
 
 MONGODB_PORT=$1
 MONGODB_INIT_FLAG=/etc/config/.mongodb_init_flag
-MONGODB_CMD="mongod --replSet rs0 --bind_ip_all --tlsMode requireTLS --tlsCAFile /etc/ssl/ca.crt --tlsCertificateKeyFile /etc/ssl/mongodb/mongodb.pem --port $MONGODB_PORT"
+MONGODB_CMD="mongod --replSet rs0 --bind_ip_all --tlsMode requireTLS --tlsCAFile /etc/ssl/ca.crt --tlsCertificateKeyFile /etc/ssl/mongodb/mongodb.pem --sslAllowInvalidCertificates false --port $MONGODB_PORT"
 
 if [ -s "$MONGODB_INIT_FLAG" ]; then
 	MONGODB_CMD="$MONGODB_CMD --keyFile /etc/config/mongodb-keyfile"
